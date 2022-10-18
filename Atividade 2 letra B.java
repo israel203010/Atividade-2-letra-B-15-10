fd96df0d014c368cc406b3308444abd6b3cc49af
israel203010 <111544955+israel203010@users.noreply.github.com>
2022-10-17 21:26:08 -0300

    Add files via upload

---
+++ Atividade 2 letra B.java
@@ -0,0 +1,15 @@
+import java .util.*;
+class Main {
+  public static int subtração int a, int b ){
+  return (a - b);
+    }
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner (System.in);
+    System.out.println("Digite o valor do primeiro numero:");
+    int a = entrada.nextInt();
+    System.out.println ("Digite o valor do segundo numero:");
+    int b = entrada.nextInt();
+    System.out.println ("A subtraçao de " + a +"+" +b +" é" +subtração (a, b));
+    
+  }
+}
